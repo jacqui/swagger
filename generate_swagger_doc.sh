f1abf8b9dc5638bf7d41501dc5243bacb189c20f
Jacqui Maher <jacqui.maher@nytimes.com>
2014-11-11 18:14:23 -0500

    hive-specific version of swagger gen script

--- generate_swagger_doc.sh
+++ generate_swagger_doc.sh
@@ -1,1 +1,3 @@
-go run generator.go -apiPackage="github.com/yvasiyarov/swagger/example" -mainApiFile="github.com/yvasiyarov/swagger/example/web/main.go"
+go run generator.go -apiPackage="github.com/nytlabs/hive/hive" -mainApiFile="github.com/nytlabs/hive/main.go" -basePath="http://localhost.nytimes.com:3000"
+go run generator.go -apiPackage="github.com/nytlabs/hive/hive" -mainApiFile="github.com/nytlabs/hive/main.go" -format="swagger" -basePath="http://localhost.nytimes.com:3000"
+go run web.go docs.go -port 8088
